f3a14247970465ce0d7d98b62131b5af11b3d37d
Nibey <50440208+yebiny@users.noreply.github.com>
2022-07-06 17:08:26 +0900

    Update plot_result.py

--- 02.케라스_창시자에게_배우는_딥러닝/scripts/plot_result.py
+++ 02.케라스_창시자에게_배우는_딥러닝/scripts/plot_result.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_lcurve(hists, titles, colors):
-  plt.figure(figsize=(12,4))
+def plot_lcurve(hists, titles, colors, figsize=(12,4), grid=True):
+  plt.figure(figsize=figsize)
   # loss 
   plt.subplot(121)
   for i, hist in enumerate(hists):
@@ -11,7 +11,7 @@ def plot_lcurve(hists, titles, colors):
     epochs = range(1, 1+len(loss))
     plt.plot(epochs, loss, linestyle=':', label=f'{titles[i]} train loss', c=colors[i])
     plt.plot(epochs, val_loss, marker='.', label=f'{titles[i]} valid loss', c=colors[i])
-    plt.legend();plt.grid(True);plt.xticks(epochs)
+    plt.legend();plt.grid(grid);plt.xticks(epochs)
     plt.xlabel('Epochs');plt.ylabel('Loss')
     x, y = epochs[-1], hist.history['loss'][-1]
     plt.text(x, y, np.round(y,2), c=colors[i])
@@ -24,7 +24,7 @@ def plot_lcurve(hists, titles, colors):
     val_acc = hist.history['val_acc']
     plt.plot(epochs, acc, linestyle=':', label=f'{titles[i]} train acc', c=colors[i])
     plt.plot(epochs, val_acc, marker='.', label=f'{titles[i]} valid acc', c=colors[i])
-    plt.legend();plt.grid(True);plt.xticks(epochs)
+    plt.legend();plt.grid(grid);plt.xticks(epochs)
     plt.xlabel('Epochs');plt.ylabel('Acc')
     x, y = epochs[-1], hist.history['acc'][-1]
     plt.text(x, y, np.round(y,2), c=colors[i])

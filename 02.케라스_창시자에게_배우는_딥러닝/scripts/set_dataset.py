32af7b5bdab36ee066c4e48f3a79364f24232e8b
Nibey <50440208+yebiny@users.noreply.github.com>
2022-07-07 12:41:13 +0900

    Update set_dataset.py

--- 02.케라스_창시자에게_배우는_딥러닝/scripts/set_dataset.py
+++ 02.케라스_창시자에게_배우는_딥러닝/scripts/set_dataset.py
@@ -4,7 +4,7 @@ def set_dogs_and_cats(org_dir, new_dir):
   org_img_paths = sorted(glob.glob(f'{org_dir}*jpg'))
   print(f'* found {len(org_img_paths)} images in {org_img_paths}')
   os.mkdir(new_dir)
-
+  
   for dir_type in ['train', 'valid', 'test']:
     dir_path = f'{new_dir}/{dir_type}'
     os.mkdir(dir_path)
@@ -18,7 +18,7 @@ def set_dogs_and_cats(org_dir, new_dir):
   for img_path in org_img_paths:
     info = img_path.split('/')[-1] # cat.0.jpg
     label, idx, _ = info.split('.')
-    
+
     if 0<=int(idx)<1000: target_path = f'{new_dir}/train/{label}/{info}'
     elif 1000<=int(idx)<1500: target_path = f'{new_dir}/valid/{label}/{info}'
     else: target_path = f'{new_dir}/test/{label}/{info}'

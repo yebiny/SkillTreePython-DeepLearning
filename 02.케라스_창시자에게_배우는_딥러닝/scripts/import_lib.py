27445cb88376d8b1b8a1d7fcceb87390e26036a2
Nibey <50440208+yebiny@users.noreply.github.com>
2022-07-11 10:05:03 +0900

    Update import_lib.py

--- 02.케라스_창시자에게_배우는_딥러닝/scripts/import_lib.py
+++ 02.케라스_창시자에게_배우는_딥러닝/scripts/import_lib.py
@@ -1,5 +1,7 @@
 import os, sys
 import numpy as np
+import maptlotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from tensorflow.keras import models, layers, utils, preprocessing, applications, callbacks
+import cv2

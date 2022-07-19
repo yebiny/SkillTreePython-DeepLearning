fd1ab67e186cba8c4caf457160cbfb0e01b70723
Nibey <50440208+yebiny@users.noreply.github.com>
2022-07-19 22:53:23 +0900

    Update plot_result.py

--- scripts/plot_result.py
+++ scripts/plot_result.py
@@ -22,19 +22,20 @@ def plot_lcurve(hists, titles, colors, size=(12,4), x_itv=1):
     x, y = epochs[-1], hist.history['val_loss'][-1]
     plt.text(x, y, np.round(y,2), c=colors[i])
   # acc
-  plt.subplot(122)
-  for i, hist in enumerate(hists):
-    acc = hist.history['acc']
-    val_acc = hist.history['val_acc']
-    plt.plot(epochs, acc, linestyle=':', label=f'{titles[i]} train acc', c=colors[i])
-    plt.plot(epochs, val_acc, marker='.', label=f'{titles[i]} valid acc', c=colors[i])
-    plt.legend();plt.grid(True);plt.xticks(xbins)
-    plt.xlabel('Epochs');plt.ylabel('Acc')
-    x, y = epochs[-1], hist.history['acc'][-1]
-    plt.text(x, y, np.round(y,2), c=colors[i])
-    x, y = epochs[-1], hist.history['val_acc'][-1]
-    plt.text(x, y, np.round(y,2), c=colors[i])
-  plt.show()
+  if 'acc' in hist.history
+    plt.subplot(122)
+    for i, hist in enumerate(hists):
+      acc = hist.history['acc']
+      val_acc = hist.history['val_acc']
+      plt.plot(epochs, acc, linestyle=':', label=f'{titles[i]} train acc', c=colors[i])
+      plt.plot(epochs, val_acc, marker='.', label=f'{titles[i]} valid acc', c=colors[i])
+      plt.legend();plt.grid(True);plt.xticks(xbins)
+      plt.xlabel('Epochs');plt.ylabel('Acc')
+      x, y = epochs[-1], hist.history['acc'][-1]
+      plt.text(x, y, np.round(y,2), c=colors[i])
+      x, y = epochs[-1], hist.history['val_acc'][-1]
+      plt.text(x, y, np.round(y,2), c=colors[i])
+    plt.show()
   
   
   
